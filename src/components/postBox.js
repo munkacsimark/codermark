4a7e39a7b2ff9cddd142220297aec03689955181
Mark Munkacsi <markmunkacsi@protonmail.com>
2021-08-10 22:23:48 +0400

    CSS tweaking around postboxes and labels

--- src/components/postBox.js
+++ src/components/postBox.js
@@ -40,85 +40,102 @@ const PostBox = ({ postData, viewType }) => {
 				margin: viewType === views.MOBILE ? `${rhythm(0.5)} auto` : rhythm(0.5),
 			}}>
 			<div className={style.postHeader}>
-				<Link to={slug} className={style.imageContainer}>
+				<div className={style.metaContainer}>
+					<span
+						role='img'
+						aria-label={language === 'hu' ? 'hungarian' : 'english'}
+						title={language === 'hu' ? 'hungarian' : 'english'}
+						className={style.language}
+						style={{
+							right: rhythm(0.5),
+							top: rhythm(0.5),
+							padding: rhythm(0.4),
+						}}>
+						{language === 'hu' ? huFlag : usFlag}
+					</span>
+					<div
+						className={style.labelsContainer}
+						style={{
+							padding: `calc(${rhythm(0.5)} * 2 + 40px) ${rhythm(
+								0.5
+							)} 0 ${rhythm(0.5)}`,
+						}}>
+						<Label
+							type={labelTypes.CATEGORY}
+							textValue={category}
+							style={{ padding: `${rhythm(0.1)} ${rhythm(0.3)}` }}
+						/>
+						<div
+							className={style.tagContainer}
+							style={{ marginTop: rhythm(0.5) }}>
+							{tags.map(tag => (
+								<Label
+									key={tag}
+									type={labelTypes.TAG}
+									textValue={tag}
+									style={{
+										padding: `${rhythm(0.1)} ${rhythm(0.3)}`,
+										margin: `0 ${rhythm(0.5)} ${rhythm(0.5)} 0`,
+									}}
+								/>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className={style.imageContainer}>
 					<GatsbyImage
 						className={style.image}
 						image={getImage(image)}
 						alt={imageAlt}
 					/>
-				</Link>
-
-				<span
-					role='img'
-					aria-label={language === 'hu' ? 'hungarian' : 'english'}
-					title={language === 'hu' ? 'hungarian' : 'english'}
-					className={style.language}
-					style={{
-						top: rhythm(0.5),
-						right: rhythm(0.5),
-						padding: rhythm(0.4),
-					}}>
-					{language === 'hu' ? huFlag : usFlag}
-				</span>
-				<div
-					className={style.labelsContainer}
-					style={{
-						left: rhythm(0.5),
-						bottom: rhythm(0.5),
-					}}>
-					<Label
-						type={labelTypes.CATEGORY}
-						textValue={category}
-						style={{ padding: `${rhythm(0.1)} ${rhythm(0.3)}` }}
-					/>
-					<div
-						className={style.tagContainer}
-						style={{ marginTop: rhythm(0.5) }}>
-						{tags.map(tag => (
-							<Label
-								key={tag}
-								type={labelTypes.TAG}
-								textValue={tag}
-								style={{
-									padding: `${rhythm(0.1)} ${rhythm(0.3)}`,
-									marginRight: rhythm(0.5),
-								}}
-							/>
-						))}
-					</div>
 				</div>
 			</div>
-			<h3 className={style.title} style={{ padding: `0 ${rhythm(1)}` }}>
-				<Link to={slug}>{title}</Link>
-			</h3>
-			<hr className={style.hr} />
-			<p style={{ padding: `0 ${rhythm(1)}` }}>{description}</p>
-			<hr className={style.hr} />
-			<div className={style.timeHolder}>
-				<time
-					className={style.time}
-					style={{
-						...scale(-1 / 5),
-						padding: `0 ${rhythm(0.5)}`,
-						marginBottom: updated ? 0 : rhythm(0.5),
-					}}
-					dateTime={created}>
-					<FontAwesomeIcon icon={faRocket} />{' '}
-					{new Date(created).toLocaleDateString(language)}
-				</time>
-				{updated && (
+			<div
+				className={style.postContent}
+				style={{ padding: `${rhythm(0.5)} 0` }}>
+				<h3
+					className={style.title}
+					style={{ padding: `0 ${rhythm(1)}`, marginTop: 0 }}>
+					<Link to={slug}>{title}</Link>
+				</h3>
+				<hr className={style.hr} />
+				<p style={{ padding: `0 ${rhythm(1)}` }}>{description}</p>
+				<hr className={style.hr} />
+				<div className={style.timeHolder}>
 					<time
 						className={style.time}
 						style={{
 							...scale(-1 / 5),
 							padding: `0 ${rhythm(0.5)}`,
-							marginBottom: rhythm(0.5),
+							marginBottom: updated ? 0 : rhythm(0.5),
 						}}
-						dateTime={updated}>
-						<FontAwesomeIcon icon={faPen} />{' '}
-						{new Date(updated).toLocaleDateString(language)}
+						dateTime={created}>
+						<FontAwesomeIcon icon={faRocket} />{' '}
+						{new Date(created).toLocaleDateString(language)}
 					</time>
-				)}
+					{updated && (
+						<time
+							className={style.time}
+							style={{
+								...scale(-1 / 5),
+								padding: `0 ${rhythm(0.5)}`,
+								marginBottom: rhythm(0.5),
+							}}
+							dateTime={updated}>
+							<FontAwesomeIcon icon={faPen} />{' '}
+							{new Date(updated).toLocaleDateString(language)}
+						</time>
+					)}
+				</div>
+				<Link
+					to={slug}
+					className={style.readButton}
+					style={{
+						...scale(-1 / 5),
+						padding: `${rhythm(0.1)} ${rhythm(1)}`,
+					}}>
+					Read
+				</Link>
 			</div>
 		</div>
 	)
