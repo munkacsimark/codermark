25c66e650c7b61ff4128e746afd48f6be28edad3
Mark Munkacsi <markmunkacsi@protonmail.com>
2023-05-22 04:42:53 +0400

    switch to hash router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import Projects from "./pages/Projects.vue";
@@ -7,7 +7,7 @@ import routes from "./routes";
 import "./main.css";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: routes.HOME, component: Home },
     { path: routes.PROJECTS, component: Projects },
