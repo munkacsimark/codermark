3fecb9e15db57fee0e8fde803325bb14ed291c81
Mark Munkacsi <markmunkacsi@protonmail.com>
2023-08-31 13:43:01 +0400

    update about text

--- src/components/AboutBox.vue
+++ src/components/AboutBox.vue
@@ -4,13 +4,17 @@ import routes from "../routes.js";
 
 <template>
   <h1>Hi, I&#x2019;m <span class="gradientText">Márk</span>!</h1>
-  <h2>Frontend developer.</h2>
+  <h2>Frontend/Fullstack developer.</h2>
   <p>
-    Mainly I&#x2019;m building performant client side apps designed for humans
-    in <span class="strong">web3</span> space using modern JavaScript tools like
-    <span class="strong">React</span>, <span class="strong">NextJS</span> or
-    <span class="strong">Vue</span> with <span class="strong">TypeScript</span>.
-    Check my projects <router-link :to="routes.PROJECTS">here</router-link>.
+    My primary focus is on developing high-performance client-side applications
+    using modern JavaScript tools such as <span class="strong">React</span>,
+    <span class="strong">NextJS</span> with
+    <span class="strong">TypeScript</span>. I&#x2019;m skilled in planning
+    <span class="strong">architectural designs</span> and building projects from
+    <span class="strong">ground up</span> as well as enhancing,
+    <span class="strong">optimizing</span> or
+    <span class="strong">rewriting</span> existing ones. Check my projects
+    <router-link :to="routes.PROJECTS">here</router-link>.
   </p>
 </template>
 
